c76792429e8dc7a3bd8ed2fdd414279f42d3e7ba
RuslanGon <ruslan441006@gmail.com>
2026-01-20 17:55:21 +0200

    wer

---
+++ server/eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+
+export default [
+  {
+    ...pluginJs.configs.recommended,
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
+    },
+  },
+]
--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,3 @@
-const message = 'Hello world Ruslan';
+const message = 'Hello world Ruslan!!!'
 
 console.log(message);
