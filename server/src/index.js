61330462573828b6b91c88b0be0439b7c81a3476
RuslanGon <ruslan441006@gmail.com>
2026-01-20 19:03:41 +0200

    weee

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import pino from 'pino-http';
+import cors from 'cors';
 
 const app = express();
-app.use(pino());
+app.use(pino({ autoLogging: false }));
 app.use(express.json());
+app.use(cors());
+
+
 
 app.get('/', (req, res, next) => {
   res.send('Hello world Ruslan');
 });
+
+app.use((req, res) => {
+    res.status(404).send('Opps rout is not found');
+    });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
