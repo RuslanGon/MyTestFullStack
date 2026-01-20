69564e8a845ae8a4bc113515a1bb88d4d61a20f6
RuslanGon <ruslan441006@gmail.com>
2026-01-20 18:46:31 +0200

    wer

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import pino from 'pino-http';
 
 const app = express();
+app.use(pino());
+app.use(express.json());
 
 app.get('/', (req, res, next) => {
-res.send('Hello word Ruslan');
+  res.send('Hello world Ruslan');
 });
 
-app.listen(3000, () =>{
-console.log('Server is running on port 3000');
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
