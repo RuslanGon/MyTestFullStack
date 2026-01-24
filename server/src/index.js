f8b74e3eb05b877762e6f05e6a95332ed0d3af3b
RuslanGon <ruslan441006@gmail.com>
2026-01-24 18:20:32 +0200

    were

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 import { initMongoConnection } from "./db/initMongoConnection.js";
+// import { initMySQLConnection } from "./db/initMySQLConnection.js";
 import { startServer } from "./server.js";
 
 (async() => {
 await initMongoConnection();
+// await initMySQLConnection();
 startServer();
 })();
 
