83c37790109104443c7787ebb421c6e8a49b1293
RuslanGon <ruslan441006@gmail.com>
2026-01-24 18:09:14 +0200

    wer

--- server/src/db/initMongoConnection.js
+++ server/src/db/initMongoConnection.js
@@ -6,8 +6,9 @@ export const initMongoConnection = async () => {
 const connectionLinck = `mongodb+srv://${env(ENV_VARS.DB_USER)}:${env(ENV_VARS.DB_PASSWORD)}@cluster0.lc6ql.mongodb.net/${env(ENV_VARS.DB_NAME)}?retryWrites=true&w=majority&appName=Cluster0`;
 try {
 await mongoose.connect(connectionLinck);
-console.log('Mongo connection');
+console.log('MongoDB connected');
 } catch (error) {
 console.log(error);
+throw error;
 }
 };
