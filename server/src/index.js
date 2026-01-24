fef328910a797f63f2865be8b634305a137f3862
RuslanGon <ruslan441006@gmail.com>
2026-01-24 19:11:45 +0200

    wer

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,9 @@
 import { initMongoConnection } from "./db/initMongoConnection.js";
-import { Student } from "./db/models/student.js";
 // import { initMySQLConnection } from "./db/initMySQLConnection.js";
 import { startServer } from "./server.js";
 
 (async() => {
 await initMongoConnection();
-const students = await Student.find({});
-console.log(students);
 // await initMySQLConnection();;
 startServer();
 })();
