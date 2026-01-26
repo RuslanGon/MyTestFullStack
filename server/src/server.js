6c5be3d76175ebcb2db46f88c13f44e2da613812
RuslanGon <ruslan441006@gmail.com>
2026-01-26 13:12:06 +0200

    wer

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@ import { env } from './utils/env.js';
 import { ENV_VARS } from './constants/index.js';
 import { notFoundMiddleware } from './middlewars/notFoundMiddleware.js';
 import { errorHandlerMiddleware } from './middlewars/errorHandlerMiddleware.js';
+import { getAllStudents, getStudentById } from './services/students.js';
 
 export const startServer = () => {
   const app = express();
@@ -12,9 +13,31 @@ export const startServer = () => {
   app.use(express.json());
   app.use(cors());
 
-  app.get('/', (req, res, next) => {
-    res.send('Hello world Ruslan');
+  app.get('/students', async (req, res, next) => {
+    const students = await getAllStudents();
+    res.json({
+      status: 200,
+      message: 'Get all students',
+      data: students,
+    });
   });
+
+  app.get('/students/:studentId', async (req, res, next) => {
+    const { studentId } = req.params;
+    const student = await getStudentById(studentId);
+    if (!student) {
+    return res.status(404).json({
+    status: 404,
+    message: 'Student not found',
+    });
+    }
+
+    res.json({
+    status: 200,
+    message: 'Get student by Id',
+    data: student,
+    });
+    });
 
   app.use(notFoundMiddleware);
   app.use(errorHandlerMiddleware);
---
+++ server/src/services/students.js
@@ -0,0 +1,9 @@
+import {Student} from '../db/models/student.js';
+
+export const getAllStudents = async () => {
+  return await Student.find({});
+};
+
+export const getStudentById = async (studentId) => {
+return await Student.findById(studentId);
+    };
