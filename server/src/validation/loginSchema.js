db9b7968fefcb646db660b4dbf1c58ccf2c8d2e6
RuslanGon <ruslan441006@gmail.com>
2026-02-19 12:15:56 +0200

    wer

---
+++ server/src/validation/loginSchema.js
@@ -0,0 +1,6 @@
+import Joi from 'joi';
+
+export const loginStudentSchema = Joi.object({
+  email: Joi.string().email().min(2).max(50).required(),
+  password: Joi.string().min(6).max(80).required(),
+});
---
+++ server/src/validation/registerSchema.js
@@ -0,0 +1,7 @@
+import Joi from 'joi';
+
+export const registerStudentSchema = Joi.object({
+  name: Joi.string().min(2).max(50).required(),
+  email: Joi.string().email().min(2).max(50).required(),
+  password: Joi.string().min(6).max(80).required(),
+});
