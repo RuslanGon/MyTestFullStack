380e1de3f9452ea8e0a0bf34f960df31385fab69
RuslanGon <ruslan441006@gmail.com>
2026-02-19 11:45:29 +0200

    wer

--- server/src/routers/students.js
+++ server/src/routers/students.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import { createStudentController, deleteStudentController, getAllStudentsController, getStudentByIdController, patchStudentController, putStudentController } from '../controllers/students.js';
 import { ctrlWrapper } from '../middlewars/ctrlWrapper.js';
 import { validateMongoId } from '../middlewars/validateMongoId.js';
+import { validateBody } from '../middlewars/validateBody.js';
+import { createStudentSchema } from '../validation/createStudentSchema.js';
 
 const studentsRouter = Router();
 
 studentsRouter.get('/', ctrlWrapper(getAllStudentsController));
 studentsRouter.get('/:studentId',validateMongoId('studentId'),  ctrlWrapper(getStudentByIdController));
-studentsRouter.post('/', ctrlWrapper(createStudentController));
+studentsRouter.post('/', validateBody(createStudentSchema), ctrlWrapper(createStudentController));
 studentsRouter.delete('/:studentId', validateMongoId('studentId'), ctrlWrapper(deleteStudentController));
 studentsRouter.patch('/:studentId', validateMongoId('studentId'), ctrlWrapper(patchStudentController));
 studentsRouter.put('/:studentId', validateMongoId('studentId'), ctrlWrapper(putStudentController));
