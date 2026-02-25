cdc428e0af75580e45663d434e262b62816840a1
RuslanGon <ruslan441006@gmail.com>
2026-02-25 18:48:41 +0200

    wer

---
+++ client/src/redux/students/selectors.js
@@ -0,0 +1,4 @@
+export const selectStudents = (state) => state.students.students;
+export const selectError = (state) => state.students.error;
+export const selectLoading = (state) => state.students.loading;
+
