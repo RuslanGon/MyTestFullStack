a11700782a893ab70dc9543a68cf3d977e6a7b37
RuslanGon <ruslan441006@gmail.com>
2026-03-03 10:36:54 +0200

    wre

---
+++ client/src/redux/auth/selectors.js
@@ -0,0 +1,5 @@
+export const selectAuthIsSignedIn = (state) => state.auth.isSignedIn;
+export const selectAuthToken = (state) => state.auth.token;
+export const selectAuthUserdata = (state) => state.auth.userData;
+export const selectAuthError = (state) => state.auht.error;
+export const selectAuthLoading = (state) => state.auth.loading;
