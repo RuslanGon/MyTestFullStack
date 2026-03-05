83a6de64663d7d69efecf7c00e601853fbc25d68
RuslanGon <ruslan441006@gmail.com>
2026-03-05 11:42:41 +0200

    wre

--- server/src/controllers/contacts.js
+++ server/src/controllers/contacts.js
@@ -1,4 +1,4 @@
-import { getAllContacts } from "../services/contacts.js";
+import { createContact, getAllContacts } from "../services/contacts.js";
 
 export const getAllContactsController = async (req, res, next) => {
     const contacts = await getAllContacts();
@@ -6,5 +6,15 @@ export const getAllContactsController = async (req, res, next) => {
       status: 200,
       message: 'Successfully found contacts',
       data: contacts,
+    });
+  };
+
+  export const createContactController = async (req, res) => {
+    const contact = await createContact(req.body);
+
+    res.status(201).json({
+      status: 201,
+      message: 'Contact successfully created',
+      data: contact,
     });
   };
--- server/src/routers/contacts.js
+++ server/src/routers/contacts.js
@@ -2,15 +2,13 @@ import { Router } from 'express';
 import { validateBody } from '../middlewars/validateBody.js';
 import { contactSchema } from '../validation/contactSchema.js';
 import { ctrlWrapper } from '../middlewars/ctrlWrapper.js';
-import { getAllContactsController } from '../controllers/contacts.js';
+import { createContactController, getAllContactsController } from '../controllers/contacts.js';
 
 const contactsRouter = Router();
 
 contactsRouter.get('/', validateBody(contactSchema), ctrlWrapper(getAllContactsController));
 
-contactsRouter.post('/', validateBody(contactSchema), (req, res, next) => {
-    res.json({ message: 'Contacts post' });
-});
+contactsRouter.post('/', validateBody(contactSchema), ctrlWrapper(createContactController));
 
 contactsRouter.delete('/:contactId', (req, res, next) => {
     res.json({ message: 'Contacts delete' });
